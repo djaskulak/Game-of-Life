a66fb81dff15c949337bcd3bd97ff47540ac56ce
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-03-02 11:22:41 -0800

    made files

---
+++ person.py
@@ -0,0 +1,9 @@
+import random
+
+class Person:
+  def __init__ (self, name, starting_money=10000):
+    self.name = name
+
+    self.starting_money = starting_money
+    self.current_money = current_money
+    
