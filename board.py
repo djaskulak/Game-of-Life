3ab2161f01ae33a57644087f127c61b805410123
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-03-04 09:17:20 -0800

    added game prompts

--- board.py
+++ board.py
@@ -6,7 +6,7 @@ class Board():
   def __init__(self, actions):
     self.actions = actions
   
-  actions = [select_job, select_house]
+  actions = [select_job, select_house, add_kid]
 
   def select_action(actions):
     random.choice(actions)
--- person.py
+++ person.py
@@ -8,9 +8,30 @@ class Person:
     self.name = name
     self.money = money
 
-  def add_kid (self, kids):
+  def get_name (self, name):
+    name = input("Welcome! What is your name? ")
+    return name
+
+  def add_kid (self, kids=0):
     kids = random.randint(0, 4)
     money = money - (kids * 1000)
     print ("You now have {kids} kids!")
     return money, kids
 
+  def print_attributes (self, name, money, job, salary, kids, home):
+    print ("Hello! Your name is {name}")
+    print ("Your balance is {money}")
+    print ("You are a {job} and make ${salary}")
+    print ("You have {kids} kids")
+    print ("You live in a {home}")
+
+person.get_name(name)
+
+play = True
+
+while play is True:
+  like = input("Would you like to play a turn? y/n")
+  if like == 'y':
+    board.select_action
+  else:
+    print("It was nice playing with you!")
